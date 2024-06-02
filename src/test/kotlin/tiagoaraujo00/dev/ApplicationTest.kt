4ba3214a3e952d77ec6234ef0cbbadb5ac45fa79
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-06-01 23:42:59 -0300

    test(endpoint): verify response of '/test1' with status, content type, and body

--- src/test/kotlin/tiagoaraujo00/dev/ApplicationTest.kt
+++ src/test/kotlin/tiagoaraujo00/dev/ApplicationTest.kt
@@ -6,6 +6,8 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import tiagoaraujo00.dev.plugins.*
+import kotlin.test.assertContains
+import org.junit.Assert.*
 
 class ApplicationTest {
     @Test
@@ -16,6 +18,11 @@ class ApplicationTest {
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
+        }
+        client.get("/test1").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("html", contentType()?.contentSubtype)
+            assertContains(bodyAsText(), "Hello from Ktor")
         }
     }
 }
